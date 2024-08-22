f2709e70593291a5f5e99b89b813c3676dc793be
KevinWijaya16 <kevinwijaya161207@gmail.com>
2024-08-22 15:52:51 +0700

    perubahan pada layer halaman login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,28 +24,30 @@ function Page() {
         <p className="font-semibold text-[15px] mx-16 mb-1">Masukan akun</p>
       </div>
       <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1 bg-white p-4 mx-12 my-3 rounded-xl">
+        <div className="col-span-1 bg-white p-4 mx-12 my-3 rounded-xl z-10">
           <form>
-            <div className="mb-[28px]">
+            <div className="mb-[28px] z-10">
               <input
                 className="w-[280px] mx-1 px-3 py-2 border border-gray-300 rounded-xl"
                 type="text"
                 placeholder="Nomor Whatsapp atau Email"
               />
             </div>
-            <div className="mb-[28px]">
+            <div className="mb-[28px] z-10">
               <input
                 className="w-[280px] mx-1 px-3 py-2 border border-gray-300 rounded-xl"
                 type="password"
                 placeholder="Password"
               />
             </div>
-            <button
-              type="submit"
-              className="w-[290px] h-[50px] text-white bg-[#E3B02B] py-2 rounded "
-            >
-              Login
-            </button>
+            <Link href="/">
+              <button
+                type="submit"
+                className="w-[290px] h-[50px] text-white bg-[#E3B02B] py-2 rounded"
+              >
+                Login
+              </button>
+            </Link>
           </form>
         </div>
 
